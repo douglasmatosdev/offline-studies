ae657dc2928c8dc77f8c89c4dcfd0ab63e047f38
douglasmatosdev <douglasmatosdev@gmail.com>
2021-06-29 21:55:05 -0300

    init tests with Marvel API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const url = 'http://gateway.marvel.com/v1/public/comics'
+const apiKey = ''
+const ts = ''
+const hash = ''
+const headers = { Headers: { Accept: '*/*' } }
+
+const credentials = { apiKey, ts, hash, headersd, headers }
+
 const App = () => {
+    // fetch(url, credentials)
+
     return (
         <AppContainer>
             <h1>Learn about applications offline</h1>
